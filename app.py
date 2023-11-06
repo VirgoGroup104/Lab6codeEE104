b407fa33dad83e1cb89629ba01879e7dcb125eea
VirgoGroup104 <149981888+VirgoGroup104@users.noreply.github.com>
2023-11-05 22:51:03 -0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,35 @@
+import os
+
+import openai
+from flask import Flask, redirect, render_template, request, url_for
+
+app = Flask(__name__)
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+
+@app.route("/", methods=("GET", "POST"))
+def index():
+    if request.method == "POST":
+        animal = request.form["animal"]
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=generate_prompt(animal),
+            temperature=0.6,
+        )
+        return redirect(url_for("index", result=response.choices[0].text))
+
+    result = request.args.get("result")
+    return render_template("index.html", result=result)
+
+
+def generate_prompt(animal):
+    return """Suggest three names for an animal that is a superhero.
+
+Animal: Tuan
+Names: Quyen Truong,Lan Tran,Thuy Vo,Ngoc Nguyen
+Animal: Khai
+Names: Selina Nguyen, Thi Nguyen, Kieu Le, Trang Pham
+Animal: {}
+Names:""".format(
+        animal.capitalize()
+    )
